d313cb73b1703898a499f4cf1041385f21984ce2
Rodrigo Bossini <professorbossini@gmail.com>
2023-05-01 14:27:31 -0300

    apostila01(chatgpt-python): funcao para criacao de perguntas

---
+++ app.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+#exibindo o valor associado à chave OPENAI_API_KEY
+#print (os.getenv('OPENAI_API_KEY'))
---
+++ chatgpt.py
@@ -0,0 +1,31 @@
+import openai
+
+def criar_pergunta(
+  OPENAI_API_KEY,
+  assunto,
+  tipo,
+  dificuldade,
+  questao_exemplo = None
+):
+  openai.api_key = OPENAI_API_KEY
+  assunto = f'Elabore uma questão sobre {assunto}'
+  tipo = f'Ela deve ser {tipo}'
+  dificuldade = f'Seu nível de dificuldade deve ser {dificuldade}'
+  questao_exemplo = f'Utilize esta questão como exemplo: {questao_exemplo}.' if questao_exemplo != None else ''
+  prompt = f'{assunto}.{tipo}.{dificuldade}.{questao_exemplo}'
+  resposta = openai.Completion.create(
+    engine='text-davinci-003',
+    prompt = prompt,
+    max_tokens=150
+  )
+  return resposta.choices[0].text.strip()
+
+
+print(
+  criar_pergunta(
+    'java',
+    'objetiva',
+    'médio',
+    
+  )
+)
