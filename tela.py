395d2b714f48ad572a44d30147373358f8c6c970
Rodrigo Bossini <professorbossini@gmail.com>
2023-05-01 19:11:08 -0300

    apostila01(chatgpt-python): novo teste de tela, passo a passo

--- tela.py
+++ tela.py
@@ -1,5 +1,13 @@
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-ttk.Button(root, text="Hello, World").grid()
-root.mainloop()
+janela_principal = Tk()
+#padding: distância entre borda e conteúdo. Ordem: Left, Top, Right, Bottom
+estilo = ttk.Style()
+estilo.configure("meu_estilo", borderwidth=2, relief='solid')
+
+frame = ttk.Frame(janela_principal, padding="12 12 12 12", style="meu_estilo")
+frame.grid(column=0, row=0)
+feet = StringVar()
+feet_entry = ttk.Entry(frame, width=12, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+janela_principal.mainloop()
