83c5a81b091bfe543d4f39a065708cadd7375b1f
Rodrigo Bossini <professorbossini@gmail.com>
2023-05-05 15:02:21 -0300

    sem prints

--- chatgpt.py
+++ chatgpt.py
@@ -7,15 +7,12 @@ def criar_pergunta(
   dificuldade,
   pergunta_exemplo = None
 ):
-  print ('chatgpt', OPENAI_API_KEY, assunto, tipo, dificuldade, pergunta_exemplo != None)
   openai.api_key = OPENAI_API_KEY
   assunto = f'Elabore uma pergunta sobre {assunto}'
   tipo = f'Ela deve ser {tipo}' + ' com 4 alternativas' if tipo == 'Alternativa' else ''
   dificuldade = f'Seu nível de dificuldade deve ser {dificuldade}'
-  print('pergunta_exemplo', pergunta_exemplo == None)
   pergunta_exemplo = f'Utilize esta pergunta como exemplo: {pergunta_exemplo}' if pergunta_exemplo != None and pergunta_exemplo != '\n' else ''
   prompt = f'{assunto}.{tipo}.{dificuldade}.{pergunta_exemplo}'
-  print('prompt', prompt)
   resposta = openai.Completion.create(
     engine='text-davinci-003',
     prompt = prompt,
